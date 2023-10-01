09c2751e4283de760af591833fd178b413de350f
Mohan Puttamadaiah <mohan.p3@gmail.com>
2023-10-01 17:21:49 +0100

    Initial commit

---
+++ src/main/java/com/kb/payment/kbpaymentsystem/KbPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kb.payment.kbpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbPaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbPaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kb/payment/kbpaymentsystem/KbPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kb.payment.kbpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

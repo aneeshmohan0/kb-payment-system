ef6ce47d8637d39883296426dd6293b2cb9a46ed
Mohan Puttamadaiah <mohan.p3@gmail.com>
2023-10-03 15:14:12 +0100

    business logic.

---
+++ src/main/java/com/kb/payment/policy/Policy.java
@@ -0,0 +1,19 @@
+package com.kb.payment.policy;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Policy {
+
+    //Payment Request coming into the Policy.
+
+    //Validate the input data
+
+    //Save the data
+
+    //Send payment link to user via e-mail.
+
+    //Once the payment is made, pull the data from Bank API or webhook.
+
+    //And Mark them as payed and send the ticket confirmation.
+}
